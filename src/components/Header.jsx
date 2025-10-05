901ce8bb04620fc1e566fc5c64b0d0b4cf0651be
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2025-10-05 11:32:06 -0700

    refactor(header): swap logo files and update import path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import logo from "./../assets/logo2.svg";
+import logo from "./../assets/logo.svg";
 import avatar from "./../assets/avatar.png";
 
 const currentDate = new Date().toLocaleString("default", {
