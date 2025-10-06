7e25e910ea7ebba0e2c8f928c9bb67a09895b69e
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2025-10-05 17:11:45 -0700

    feat(footer): implement Footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import AppContainer from "./AppContainer";
 import Header from "./Header";
 import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <AppContainer>
         <Header />
         <Main />
+        <Footer />
       </AppContainer>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,12 @@
-function Footer() {}
+function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <span className="footer__author">Troy Coffindaffer</span>
+      <span className="footer__current-year">{year}</span>
+    </footer>
+  );
+}
 
 export default Footer;
