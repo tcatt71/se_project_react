d7e771c50493f9cc0c9a92fe1a7b23c3b1b9af45
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-04-19 17:29:24 -0700

    refactor api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,25 +1,13 @@
 const baseUrl = "http://localhost:3001";
 
 function getItems() {
-  return fetch(`${baseUrl}/items`).then((res) => {
-    if (!res.ok) {
-      throw new Error(`Failed to fetch items: ${res.status}`);
-    } else {
-      return res.json();
-    }
-  });
+  return fetch(`${baseUrl}/items`).then(processResponse);
 }
 
 function deleteItem(id) {
   return fetch(`${baseUrl}/items/${id}`, {
     method: "DELETE",
-  }).then((res) => {
-    if (!res.ok) {
-      throw new Error(`Failed to delete item: ${res.status}`);
-    } else {
-      return res.json();
-    }
-  });
+  }).then(processResponse);
 }
 
 function addItem(item) {
@@ -27,12 +15,14 @@ function addItem(item) {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(item),
-  }).then((res) => {
-    if (!res.ok) {
-      throw new Error(`Failed to POST new item: ${res.status}`);
-    }
-    return res.json();
-  });
+  }).then(processResponse);
+}
+
+function processResponse(res) {
+  if (!res.ok) {
+    throw new Error(`Error: ${res.status} ${res.statusText}`);
+  }
+  return res.json();
 }
 
 export { getItems, deleteItem, addItem };
