ddfb6ccfb43e4dea057110b9ce06696ca3f59602
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-07-08 20:35:10 -0700

    refactor: use named imports

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,9 +1,9 @@
-import * as constants from "./constants.js";
+import { LATITUDE, LONGITUDE, API_KEY } from "./constants.js";
 import { request } from "./api.js";
 
 function fetchData() {
   return request(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${constants.LATITUDE}&lon=${constants.LONGITUDE}&units=imperial&appid=${constants.API_KEY}`,
+    `https://api.openweathermap.org/data/2.5/weather?lat=${LATITUDE}&lon=${LONGITUDE}&units=imperial&appid=${API_KEY}`,
   );
 }
 
