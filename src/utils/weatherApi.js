78705e6669ca2c10276e93c3d1db9e3f0c17c72a
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-07-12 02:25:55 -0700

    refactor: remane coordinate constants

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,9 +1,9 @@
-import { LATITUDE, LONGITUDE, API_KEY } from "./constants.js";
+import { DEFAULT_LATITUDE, DEFAULT_LONGITUDE, API_KEY } from "./constants.js";
 import { request } from "./api.js";
 
 function fetchData() {
   return request(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${LATITUDE}&lon=${LONGITUDE}&units=imperial&appid=${API_KEY}`,
+    `https://api.openweathermap.org/data/2.5/weather?lat=${DEFAULT_LATITUDE}&lon=${DEFAULT_LONGITUDE}&units=imperial&appid=${API_KEY}`,
   );
 }
 
