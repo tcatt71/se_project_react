aadeee5d04c709275eaa6163d864a33ad6cfa123
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-07-15 03:36:07 -0700

    feat: create register function to sign up users

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,1 +1,12 @@
 import { BASE_URL } from "./constants.js";
+import { request } from "./api.js";
+
+function register({ name, avatar, email, password }) {
+  return request(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ name, avatar, email, password }),
+  });
+}
+
+export { register };
