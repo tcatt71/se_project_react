5794451c6290eaa8e54ffeb80b2c3e65df2f671f
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-07-26 00:23:05 -0700

    feat: add profile details on login

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,13 +1,21 @@
-import avatar from "./../../assets/avatar.png";
-
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import "./SideBar.css";
 
 function SideBar() {
+  const { currentUser } = useContext(CurrentUserContext);
+  const { avatar, name } = currentUser || {};
+  const userInitial = name?.trim()?.charAt(0);
+
   return (
     <div className="side-bar">
       <div className="side-bar__container">
-        <img className="side-bar__avatar" src={avatar} alt="User" />
-        <span className="side-bar__user-name">Terrence Tegegne</span>
+        {avatar ? (
+          <img className="side-bar__avatar" src={avatar} alt="User" />
+        ) : (
+          <div className="side-bar__avatar">{userInitial?.toUpperCase()}</div>
+        )}
+        <span className="side-bar__user-name">{name}</span>
       </div>
     </div>
   );
